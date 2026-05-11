6a5bf2ce28da21cc8c3868ebace8819b42f623a3
Krzysztof <krzysztof@engineer.com>
2026-05-11 14:18:03 +0200

    Add Google Analytics tracking code

--- resources/views/google-analytics.blade.php
+++ resources/views/google-analytics.blade.php
@@ -1,1 +1,9 @@
-{{-- Replace this view with your analytics snippet --}}
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-7910Z4THZ1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-7910Z4THZ1');
+</script>
